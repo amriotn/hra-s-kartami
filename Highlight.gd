306b633ec3f756ed8e6f7a0dd67977f7613975a8
amriotn <maca.dol.13@seznam.cz>
2023-12-15 16:55:23 +0100

    Oprava Highlight Tile

--- Highlight.gd
+++ Highlight.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,11 +10,8 @@ func _ready():
 func _process(delta):
 	pass
 
-func _input(event):
-	if event is InputEventMouseButton and event.is_action_pressed("left_click"):
-		if get_viewport_rect().has_point(to_local(event.position)):
-			print("You selected:", self.name)
 
-func _unhandled_input(event):
-	if event is InputEventMouseButton and event.is_action_pressed("left_click"):
-		print("You selected:")
+
+func _on_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton && event.pressed):
+		print('Clicked')
