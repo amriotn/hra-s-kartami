4cb7e93a2146ecb8c91e1f2b2c3ace7d3428fc6a
amriotn <maca.dol.13@seznam.cz>
2024-03-07 10:39:26 +0100

    StuckPlayer a Crossroads finished
    StuckPlayer funkční
    Crossroads možná zkrášlit trochu

--- scenes/main/ActionsOfTiles.gd
+++ scenes/main/ActionsOfTiles.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+var tile_node : TileMap
 var actions_array : Array
 var move_player : Dictionary
 var stuck_player : Dictionary
-var tile_node : TileMap
+var crossroads : Dictionary
+var give_card : Dictionary
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,13 +16,13 @@ func _ready():
 	stuck_player = {}
 	
 	for action in actions_array:
-		print("\t"+action.name)
+		print(action.name)
 		# = MovePlayer; StuckPlayer; GiveCard
 		match action.name:
 				"MovePlayer":
 					for action_tile : Node2D in action.get_children():
 						# = DivotvornyMec; ...
-						print(action_tile.name)
+						print("\t"+action_tile.name)
 						var actions_markers : Array = action_tile.get_children()
 						# FIX MARKER POSITION TO CENTER OF TILE
 						for marker : Marker2D in actions_markers:
@@ -29,11 +31,29 @@ func _ready():
 						move_player[actions_markers[0].global_position] = actions_markers[1].global_position
 				"StuckPlayer":
 					for action_tile in action.get_children():
+						# = San_Roll3, ...
+						print("\t"+action_tile.name)
 						var dice_number_needed = int(String(action_tile.name)[-1])
-						var actions_markers : Array = action_tile.get_children()
+						var actions_markers : Array = action_tile.get_children() # All tiles player can be stuck on
+						for marker : Marker2D in actions_markers:
+							# fix marker positon to center of tile
+							marker.global_position = tile_node.map_to_local(tile_node.local_to_map(marker.global_position))
+							# on this tile, player will be stuck until he throws => dice_number_needed
+							stuck_player[marker.global_position] = dice_number_needed
+				"Crossroads":
+					for action_tile in action.get_children():
+						# Jezibaba, ...
+						print("\t"+action_tile.name)
+						var actions_markers : Array = action_tile.get_children() # StartMarker, OddWay, EvenWay
+						
+						# fix marker positon to center of tile
 						for marker : Marker2D in actions_markers:
 							marker.global_position = tile_node.map_to_local(tile_node.local_to_map(marker.global_position))
-							stuck_player[marker.global_position] = dice_number_needed
+						
+						# crossroads = { (StartMarker) : ([OddWay, EvenWay, StraightWay]) }
+						if actions_markers.size() == 4:
+							crossroads[actions_markers[0].global_position] = [actions_markers[1].global_position, actions_markers[2].global_position, actions_markers[3].global_position]
 	
 	print(move_player)
 	print(stuck_player)
+	print(crossroads)
--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -3,11 +3,13 @@ extends Sprite2D
 @onready var highlight_scene : PackedScene = load("res://scenes/highlight/Highlight.tscn")
 @onready var rolldice_button : Button = self.get_parent().get_node("MainCamera/GUI/CenterContainer/DiceControl/RollDice_button")
 
-var dice_number : int = 0
-var highlights : Array = []
-
 var tilemap_node : TileMap
 var actions_of_tiles : Node2D
+
+var dice_number : int = 0
+var highlights : Array = []
+var stuck_until_dice_number : int = 0
+
 
 
 func _on_dice_number_send_dice_number(number):
@@ -16,20 +18,33 @@ func _on_dice_number_send_dice_number(number):
 	var tilemap_node : TileMap = self.get_parent().get_node("TileMap")
 	
 	var reference_tile = tilemap_node.local_to_map(self.position)
-	check_surround_tiles_help(reference_tile, reference_tile, dice_number)
+	
+	if stuck_until_dice_number == 0 or dice_number == stuck_until_dice_number:
+		check_surround_tiles_help(reference_tile, reference_tile, dice_number)
+		stuck_until_dice_number = 0
 	
 
 func check_surround_tiles_help(center_tile : Vector2i, previous_tile : Vector2i, dice_number : int):
 	tilemap_node = self.get_parent().get_node("TileMap")
+	actions_of_tiles = self.get_parent().get_node("ActionsOfTiles")
+	
 	var current_surrounds : Array = tilemap_node.get_surrounding_cells(center_tile) # Získá souřadnice okolních polí
 	#print("Tile ", center_tile, "surronds: ", current_surrounds,"\ndice:", dice_number)
 	rolldice_button.disabled = true
 	
 	if dice_number > 0:
-		for tile in current_surrounds:
-			if tile != previous_tile:
-				if tilemap_node.get_cell_source_id(0, tile) == 0: # = tile je na Layer 0 a je z TileSet 0
-					check_surround_tiles_help(tile, center_tile, dice_number-1)
+		if actions_of_tiles.crossroads.has(tilemap_node.map_to_local(center_tile)): # center_tile is a crossroad
+			if dice_number == self.dice_number: # STRAIGHT WAY
+				check_surround_tiles_help(tilemap_node.local_to_map(actions_of_tiles.crossroads.get(tilemap_node.map_to_local(center_tile))[2]), tilemap_node.local_to_map(actions_of_tiles.crossroads.get(tilemap_node.map_to_local(center_tile))[2]), dice_number-1)
+			elif self.dice_number%2 == 0: # EVEN WAY
+				check_surround_tiles_help(tilemap_node.local_to_map(actions_of_tiles.crossroads.get(tilemap_node.map_to_local(center_tile))[1]), tilemap_node.local_to_map(actions_of_tiles.crossroads.get(tilemap_node.map_to_local(center_tile))[1]), dice_number-1)
+			else: # ODD WAY
+				check_surround_tiles_help(tilemap_node.local_to_map(actions_of_tiles.crossroads.get(tilemap_node.map_to_local(center_tile))[0]), tilemap_node.local_to_map(actions_of_tiles.crossroads.get(tilemap_node.map_to_local(center_tile))[0]), dice_number-1)
+		else:
+			for tile in current_surrounds:
+				if tile != previous_tile:
+					if tilemap_node.get_cell_source_id(0, tile) == 0: # = tile je na Layer 0 a je z TileSet 0
+						check_surround_tiles_help(tile, center_tile, dice_number-1)
 	elif dice_number <= 0:
 		var highlight = highlight_scene.instantiate()
 		tilemap_node.add_child(highlight)
@@ -62,5 +77,11 @@ func handle_what_tile_player_stepped_on():
 	elif actions_of_tiles.stuck_player.has(self.position):
 		print("stuck player works")
 		print(actions_of_tiles.stuck_player.get(self.position))
+		stuck_until_dice_number = actions_of_tiles.stuck_player.get(self.position)
 	
+	elif actions_of_tiles.crossroads.has(self.position):
+		print("crossroads work")
+		print(actions_of_tiles.crossroads.get(self.position))
 	
+	elif actions_of_tiles.give_card.has(self.position):
+		print("give card works")
